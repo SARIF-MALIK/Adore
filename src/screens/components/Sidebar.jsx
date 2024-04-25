e8fb913feac9d7b3284f932279f4096f24f9010b
Jitesh <jiteshtripathi16480@gmail.com>
2024-04-25 20:53:46 +0530

    Implemented ActiveClass

--- src/screens/components/Sidebar.jsx
+++ src/screens/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { IoHomeOutline } from "react-icons/io5";
 import { LiaLuggageCartSolid } from "react-icons/lia";
 import { IoIosStats } from "react-icons/io";
@@ -7,29 +8,46 @@ import { IoLogoDropbox } from "react-icons/io";
 import { LuClipboardSignature } from "react-icons/lu";
 import { CiSettings } from "react-icons/ci";
 import { IoIosLogOut } from "react-icons/io";
-import { Link } from 'react-router-dom';
+
 
 function Sidebar() {
   return (
     <div className='flex flex-col h-full w-full justify-between p-5 border rounded-e-lg bg-white'>
-        <ul className='poppins-5 flex flex-col'>
-         <Link to='/' className='poppins-6 text-xl text-[#1570EF] mb-10 mx-2'>CMS</Link>
-            <Link to="/dashboard" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><IoHomeOutline size={24} />Dashboard</Link>
-            <Link to="/inventory" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><LiaLuggageCartSolid size={24} />Inventory</Link>
-            <Link to="/reports" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><IoIosStats size={24} />Reports</Link>
-            <Link to="/suppliers" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><FaRegUserCircle size={24} />Suppliers</Link>
-            <Link to="/orders" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><IoLogoDropbox size={24} />Orders</Link>
-            <Link to="/branch" className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><LuClipboardSignature size={24} />Manage Branch</Link>
-        </ul>
+      <ul className='poppins-5 flex flex-col'>
+        <NavLink exact to="/" className='poppins-6 text-xl text-[#1570EF] mb-10 mx-2' >CMS</NavLink>
+        <CustomNavLink to="/dashboard"><IoHomeOutline size={24} />Dashboard</CustomNavLink>
+        <CustomNavLink to="/inventory"><LiaLuggageCartSolid size={24} />inventory</CustomNavLink>
+        <CustomNavLink to="/reports"><IoIosStats size={24} />reports</CustomNavLink>
+        <CustomNavLink to="/suppliers"><FaRegUserCircle size={24} />suppliers</CustomNavLink>
+        <CustomNavLink to="/orders"><IoLogoDropbox size={24} />orders</CustomNavLink>
+        <CustomNavLink to="/branch"><LuClipboardSignature size={24} />branch</CustomNavLink>
+      </ul>
 
-        <div>
-            <ul className='poppins-5'>
-                <Link to='/settings' className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><CiSettings size={24} />Settings </Link>
-                <Link to='/' className='flex my-5 gap-3 md:mx-3 cursor-pointer hover:text-[#1570EF]'><IoIosLogOut size={24} />Log Out</Link>
-            </ul>
-        </div>
+      <div>
+        <ul className='poppins-5'>
+          <NavLink to='/settings' className='flex my-5 gap-3 md:mx-3 cursor-pointer' activeClassName='text-[#1570EF]'><CiSettings size={24} />Settings</NavLink>
+          <NavLink exact to='/' className='flex my-5 gap-3 md:mx-3 cursor-pointer' activeClassName='text-[#1570EF]'><IoIosLogOut size={24} />Log Out</NavLink>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+
+export default Sidebar;
+
+
+const CustomNavLink = ({ to, children }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive
+          ? "flex my-5 gap-3 md:mx-3 cursor-pointer text-[#1570EF]"
+          : "flex my-5 gap-3 md:mx-3 cursor-pointer"
+      }
+    >
+      {children}
+    </NavLink>
+  )
+}
