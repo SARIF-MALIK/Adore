fc1d868f2ea567f411a55198c8d0166da770796e
Abhishek-sharma18 <abhisheksharmac277@gmail.com>
2024-05-06 20:58:05 +0530

    made changes in json

--- server/models/products.model.js
+++ server/models/products.model.js
@@ -1,25 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    productName:{
-        type:String, 
-        required:true, 
-        trim:true,
-        lowercase: true,
-    },
-    category:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Category'
-    },
-    productID:{
-        type: Number, 
-        required: true,
-    },
-    productImg:{
-        type:String,
-        trim:true,
-    },
-})
+  productName: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  productID: {
+    type: Number,
+    required: true,
+  },
+  productImg: {
+    type: String,
+    trim: true,
+  },
+});
 
-export default mongoose.model("Product", productSchema);  
- 
+export default mongoose.model("Product", productSchema);
