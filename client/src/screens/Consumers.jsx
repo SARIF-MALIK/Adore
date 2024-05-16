b12cf8b70f1811ee8a56526ecaf04299fb33d668
Jitesh <jiteshtripathi16480@gmail.com>
2024-05-16 20:23:32 +0530

    added "Add Consumer" Form in consumers page

--- client/src/screens/Consumers.jsx
+++ client/src/screens/Consumers.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 import ConsumerInfo from './components/ConsumerInfo'
+import AddProduct from './components/AddProduct'
+import AddConsumers from './components/AddConsumers'
 
 const Consumers = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <div className="w-screen h-screen flex bg-[#F0F1F3] relative">
-            <div className={`w-2/12 h-screen `}>
+            <div className={`w-2/12 h-screen ${toggle ? 'opacity-50' : ''}`}>
                 <Sidebar />
             </div>
             <div className={`w-10/12 flex flex-col gap-3 px-5 overflow-y-auto `}>
@@ -14,8 +18,13 @@ const Consumers = () => {
                     <Navbar />
                 </div>
                 <div className>
-                    <ConsumerInfo />
+                    <ConsumerInfo toggle={toggle} setToggle={setToggle} />
                 </div>
+            </div>
+            <div>
+                {
+                    toggle && <AddConsumers />
+                }
             </div>
         </div>
     )
